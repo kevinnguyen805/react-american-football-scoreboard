6a23f529959b6fdad0256868189c0304ab91e856
admin <admin@admins-MacBook-Pro.local>
2019-09-09 11:37:59 -0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => updateHomeScore(homeScore + 6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => updateHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={ () => updateHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => updateAwayScore(awayScore + 6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => updateAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => updateAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
       </section>
