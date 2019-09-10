ccf5e3741689205fb9cdec3c5cf374387d123df9
admin <admin@admins-MacBook-Pro.local>
2019-09-10 07:59:49 -0700

    updated component breakdown

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-import ScoreButton from "./ScoreButton"
+import ScoreButton from "./ScoreButton";
+import Score from "./Score"
 
 function App() {
 
@@ -12,20 +13,7 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
+        <Score home={homeScore} away={awayScore}/>
         <BottomRow />
       </section>
         <ScoreButton home={homeScore} updateHome={updateHomeScore} away={awayScore} updateAway={updateAwayScore}  />
