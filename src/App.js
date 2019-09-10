e99e13247f2f8d1582efa6e501168542ac56eebb
admin <admin@admins-MacBook-Pro.local>
2019-09-09 22:10:00 -0700

    stretch breaking components into smaller components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-
+import ScoreButton from "./ScoreButton"
 
 function App() {
 
   const [homeScore, updateHomeScore] = useState(0);
   const [awayScore, updateAwayScore] = useState(0);
-
-
-  // stretch handler function 
- 
-
 
   return (
     <div className="container">
@@ -33,17 +28,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => updateHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => updateHomeScore(homeScore + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => updateAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => updateAwayScore(awayScore + 3)}>Away Field Goal</button>
-        </div>
-      </section>
+        <ScoreButton home={homeScore} updateHome={updateHomeScore} away={awayScore} updateAway={updateAwayScore}  />
     </div>
   );
 }
